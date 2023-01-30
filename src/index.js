c0aae82d32dee31bf8314b24e9bbc811e4a8dbe6
Timo Schwarzer <me@timoschwarzer.com>
2023-01-30 21:46:03 +0100

    We are ori-community now

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ server.get('/releases', async (req, res) => {
   res.send(await cache.retrieve('releases', RELEASES_TTL, async () => {
     const octokit = new Octokit
     return (await octokit.rest.repos.listReleases({
-      owner: 'ori-rando',
-      repo: 'build',
+      owner: 'ori-community',
+      repo: 'rando-build',
       per_page: 100,
     })).data
   }))
@@ -26,7 +26,7 @@ server.get('/motd/wotw', async (req, res) => {
   res.send(await cache.retrieve('wotw-motd', MOTD_TTL, async () => {
     const octokit = new Octokit
     const base64Content = (await octokit.rest.repos.getContent({
-      owner: 'ori-rando',
+      owner: 'ori-community',
       repo: 'motd',
       path: 'motd.wotw.html'
     })).data.content
