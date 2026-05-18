72256b31820420ade235caae5e3a626c77ad851a
Timo Schwarzer <timo@schwarzer.dev>
2026-05-18 18:23:04 +0200

    Generify cache operations

--- src/application_state.rs
+++ src/application_state.rs
@@ -1,4 +1,5 @@
-use cached::TimedCache;
+use cached::{Cached, TimedCache};
+use std::fmt::Display;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
@@ -20,6 +21,38 @@ impl ApplicationState {
             responses_cache: Arc::new(Mutex::new(TimedCache::with_lifespan(Duration::from_hours(
                 24,
             )))),
+        }
+    }
+
+    /// Returns a cached value for `entry`. If no value exists, runs the future returned by `f`
+    /// and stores its result in the cache. If the future returns an error result, nothing is
+    /// written to cache and the result is returned as-is.
+    pub async fn get_or_set_cache_with<F, FUT, E>(
+        &self,
+        entry: CacheEntry,
+        f: F,
+    ) -> Result<String, E>
+    where
+        F: FnOnce() -> FUT,
+        FUT: Future<Output = Result<String, E>>,
+        E: Display,
+    {
+        let cached_response = self.responses_cache.lock().await.cache_get(&entry).cloned();
+
+        if let Some(cached_response_body) = cached_response {
+            Ok(cached_response_body)
+        } else {
+            match f().await {
+                Ok(response_body) => {
+                    self.responses_cache
+                        .lock()
+                        .await
+                        .cache_set(CacheEntry::WotwMotd, response_body.clone());
+
+                    Ok(response_body)
+                }
+                Err(error) => Err(error),
+            }
         }
     }
 }
--- src/http/wotw_motd.rs
+++ src/http/wotw_motd.rs
@@ -3,46 +3,29 @@ use axum::body::Body;
 use axum::extract::State;
 use axum::http::{HeaderValue, StatusCode};
 use axum::response::{IntoResponse, Response};
-use cached::Cached;
 
 pub async fn handler(State(state): State<ApplicationState>) -> Response {
-    let cached_response = state
-        .responses_cache
-        .lock()
-        .await
-        .cache_get(&CacheEntry::WotwMotd)
-        .cloned();
-
-    let response_body = if let Some(cached_response_body) = cached_response {
-        cached_response_body
-    } else {
+    let response_body = match state.get_or_set_cache_with(CacheEntry::WotwReleases, async || -> Result<String, octocrab::Error> {
         let octocrab = octocrab::instance();
 
         let response_body = serde_json::to_string(
-            &match octocrab
+            &octocrab
                 .repos("ori-community", "motd")
                 .get_content()
                 .path("motd.wotw.html")
                 .send()
-                .await
-            {
-                Ok(x) => x,
-                Err(err) => {
-                    log::error!("Error fetching WotW MotD from GitHub: {}", err);
-                    return StatusCode::INTERNAL_SERVER_ERROR.into_response()
-                },
-            }
-            .items,
+                .await?
+                .items,
         )
-        .unwrap();
+            .unwrap();
 
-        state
-            .responses_cache
-            .lock()
-            .await
-            .cache_set(CacheEntry::WotwMotd, response_body.clone());
-
-        response_body
+        Ok(response_body)
+    }).await {
+        Ok(body) => body,
+        Err(err) => {
+            log::error!("Error fetching WotW MotD from GitHub: {}", err);
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response()
+        }
     };
 
     let mut response = Response::new(Body::new(response_body));
