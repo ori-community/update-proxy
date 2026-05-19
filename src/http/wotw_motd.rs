a4be9f06dcf29d1a05935150ae85c1eff25d635e
Timo Schwarzer <timo@schwarzer.dev>
2026-05-19 09:51:19 +0200

    Fix response format of the WotW MotD endpoint

--- src/http/wotw_motd.rs
+++ src/http/wotw_motd.rs
@@ -3,32 +3,51 @@ use axum::body::Body;
 use axum::extract::State;
 use axum::http::{HeaderValue, StatusCode};
 use axum::response::{IntoResponse, Response};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MotdResponse {
+    motd: String,
+}
 
 pub async fn handler(State(state): State<ApplicationState>) -> Response {
-    let response_body = match state.get_or_set_cache_with(CacheEntry::WotwReleases, async || -> Result<String, octocrab::Error> {
-        let octocrab = octocrab::instance();
+    let response_body = match state
+        .get_or_set_cache_with(
+            CacheEntry::WotwReleases,
+            async || -> Result<String, octocrab::Error> {
+                let octocrab = octocrab::instance();
 
-        let response_body = serde_json::to_string(
-            &octocrab
-                .repos("ori-community", "motd")
-                .get_content()
-                .path("motd.wotw.html")
-                .send()
-                .await?
-                .items,
+                let response_body = serde_json::to_string(&MotdResponse {
+                    motd: octocrab
+                        .repos("ori-community", "motd")
+                        .get_content()
+                        .path("motd.wotw.html")
+                        .send()
+                        .await?
+                        .items
+                        .into_iter()
+                        .next()
+                        .unwrap()
+                        .decoded_content()
+                        .unwrap(),
+                })
+                .unwrap();
+
+                Ok(response_body)
+            },
         )
-            .unwrap();
-
-        Ok(response_body)
-    }).await {
+        .await
+    {
         Ok(body) => body,
         Err(err) => {
             log::error!("Error fetching WotW MotD from GitHub: {}", err);
-            return StatusCode::INTERNAL_SERVER_ERROR.into_response()
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
         }
     };
 
     let mut response = Response::new(Body::new(response_body));
-    response.headers_mut().insert("Content-Type", HeaderValue::from_static("application/json"));
+    response
+        .headers_mut()
+        .insert("Content-Type", HeaderValue::from_static("application/json"));
     response
 }
